8983d6329e634e8a4796c1c5274e86dc1b436e54
Jason Khong <jason@jasonkhong.com>
2010-10-30 09:51:34 +0800

    Initial HelloWorld project

---
+++ Classes/HelloWorldViewController.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Jason Khong on 10/29/10.
+//  Copyright 2010 Apptivity Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+	IBOutlet UILabel *newLabel;
+}
+
+- (IBAction)sayByeBye:(id)sender;
+
+@end
+
---
+++ Classes/HelloWorldViewController.m
@@ -0,0 +1,69 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorld
+//
+//  Created by Jason Khong on 10/29/10.
+//  Copyright 2010 Apptivity Lab. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@implementation HelloWorldViewController
+
+
+
+/*
+// The designated initializer. Override to perform setup that is required before the view is loaded.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+}
+*/
+
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	newLabel.text = @"Hello Monash!";
+}
+
+- (IBAction)sayByeBye:(id)sender
+{
+	newLabel.text = @"See ya, Monash!";
+}
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
